e130e45ef4508a427cfa37c3d0517e655abc439f
Ninja Hound <60533346+NinjaTG@users.noreply.github.com>
2021-06-08 22:13:34 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -193,7 +193,7 @@ class MirrorListener(listeners.MirrorListeners):
             else:
                 uname = f'<a href="tg://user?id={self.message.from_user.id}">{self.message.from_user.first_name}</a>'
             if uname is not None:
-                msg += f'\n\n<b>👤 Uploader: </b>👉 {uname}\n\n➩ 𝖲𝗍𝗋𝖾𝖺𝗆𝗂𝗇𝗀 𝖫𝗂𝗇𝗄: 📽️ <a href="{share_urls}">𝗢𝗡𝗟𝗜𝗡𝗘 𝗪𝗔𝗧𝗖𝗛</a> \n\n⛔ 𝘿𝙤 𝙣𝙤𝙩 𝙨𝙝𝙖𝙧𝙚 𝙄𝙣𝙙𝙚𝙭 𝙇𝙞𝙣𝙠🙂 \n\n🛡️𝗣𝗼𝘄𝗲𝗿𝗲𝗱 𝗕𝘆: <b>@TGFilmZone</b>'
+                msg += f'\n\n<b>👤 Uploader: </b>👉 {uname}\n\n📽️ <a href="{share_urls}">𝗢𝗡𝗟𝗜𝗡𝗘 𝗪𝗔𝗧𝗖𝗛</a> \n\n⛔ 𝘿𝙤 𝙣𝙤𝙩 𝙨𝙝𝙖𝙧𝙚 𝙄𝙣𝙙𝙚𝙭 𝙇𝙞𝙣𝙠🙂 \n\n🛡️𝗣𝗼𝘄𝗲𝗿𝗲𝗱 𝗕𝘆: <b>@TGFilmZone</b>'
             try:
                 fs_utils.clean_download(download_dict[self.uid].path())
             except FileNotFoundError:
