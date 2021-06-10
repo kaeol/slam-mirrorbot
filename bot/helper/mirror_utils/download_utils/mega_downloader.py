9a2c0b580b1a71d21c2d0a34e85d1c2543112992
Ninja Hound <60533346+NinjaTG@users.noreply.github.com>
2021-06-10 12:20:56 +0530

    Update mega_downloader.py

--- bot/helper/mirror_utils/download_utils/mega_downloader.py
+++ bot/helper/mirror_utils/download_utils/mega_downloader.py
@@ -168,6 +168,7 @@ class MegaDownloadHelper:
             node = folder_api.authorizeNode(mega_listener.node)
         if STOP_DUPLICATE_MEGA:
             msg = sendMessage('Check the File/Folder if already in drive...', listener.bot, listener.update)
+            LOGGER.info(f'Check the File/Folder if already in drive')
             mname = node.getName()
             if listener.isTar == True:
                 mname = mname + ".tar"
@@ -184,11 +185,12 @@ class MegaDownloadHelper:
             else:
                 deleteMessage(listener.bot, msg)
         if MEGA_LIMIT is not None:
-            msg2 = sendMessage('<code>Checking the File/Folder size...</code>', listener.bot, listener.update)
+            msg2 = sendMessage('Check the File/Folder size...', listener.bot, listener.update)
+            LOGGER.info(f'Check the File/Folder size')
             limit = MEGA_LIMIT
             limit = limit.split(' ', maxsplit=1)
             limitint = int(limit[0])
-            msg3 = f'<b>Your MEGA Link Size :</b> <code>{get_readable_file_size(api.getSize(node))}</code>\n\n<b>MEGA Mirror Limit is</b>: <code>{MEGA_LIMIT}</code>.'
+            msg3 = f'Failed, Mega limit is {MEGA_LIMIT}.\nYour File/Folder size is {get_readable_file_size(api.getSize(node))}.'
             if 'GB' in limit or 'gb' in limit:
                 if api.getSize(node) > limitint * 1024**3:
                     deleteMessage(listener.bot, msg2)
@@ -203,10 +205,10 @@ class MegaDownloadHelper:
                     return
                 else:
                     deleteMessage(listener.bot, msg2)
+        sendStatusMessage(listener.update, listener.bot)
         with download_dict_lock:
             download_dict[listener.uid] = MegaDownloadStatus(mega_listener, listener)
         os.makedirs(path)
         gid = ''.join(random.SystemRandom().choices(string.ascii_letters + string.digits, k=8))
         mega_listener.setValues(node.getName(), api.getSize(node), gid)
         executor.do(api.startDownload,(node,path))
-        sendStatusMessage(listener.update, listener.bot)
