2d1f3984a188fe466128da8928dd7e6843e14cae
Ninja Hound <60533346+NinjaTG@users.noreply.github.com>
2021-06-12 09:50:20 +0530

    Update delete.py

--- bot/modules/delete.py
+++ bot/modules/delete.py
@@ -1,4 +1,4 @@
-from telegram.ext import CommandHandler, run_async
+from telegram.ext import CommandHandler
 import threading
 from telegram import Update
 from bot import dispatcher, LOGGER
@@ -6,8 +6,8 @@ from bot.helper.telegram_helper.message_utils import auto_delete_message, sendMe
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.mirror_utils.upload_utils import gdriveTools
-
-@run_async
+ 
+ 
 def deletefile(update, context):
 	msg_args = update.message.text.split(None, 1)
 	msg = ''
@@ -16,14 +16,14 @@ def deletefile(update, context):
 		LOGGER.info(msg_args[1])
 	except IndexError:
 		msg = 'Send a link along with command'
-
+ 
 	if msg == '' : 
 		drive = gdriveTools.GoogleDriveHelper()
 		msg = drive.deletefile(link)
 	LOGGER.info(f"DeleteFileCmd : {msg}")
 	reply_message = sendMessage(msg, context.bot, update)
-
+ 
 	threading.Thread(target=auto_delete_message, args=(context.bot, update.message, reply_message)).start()
-
-delete_handler = CommandHandler(command=BotCommands.DeleteCommand, callback=deletefile, filters=CustomFilters.owner_filter | CustomFilters.sudo_user)
+ 
+delete_handler = CommandHandler(command=BotCommands.DeleteCommand, callback=deletefile, filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
 dispatcher.add_handler(delete_handler)
