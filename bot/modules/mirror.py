12f0898b2a33df9c8f9cdac5cc7ca12c1582fc1e
Ninja Hound <60533346+NinjaTG@users.noreply.github.com>
2021-06-30 13:08:25 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -281,7 +281,7 @@ def _mirror(bot, update, isTar=False, extract=False):
     else:
         tag = None
     if not bot_utils.is_url(link) and not bot_utils.is_magnet(link):
-        bot.send_message(update.message.chat_id, text='<b>Read This 😐</b>\n\n➩ <a href="https://telegra.ph/HOW-TO-MIRROR-06-08"> 𝐇𝐎𝐖 𝐓𝐎 𝐌𝐈𝐑𝐑𝐎𝐑?</a>',disable_web_page_preview=True, parse_mode='HTMl')
+        bot.send_message(update.message.chat_id, text='<b>Opps 🤦! Not Like That</b>\n\nCheck 👉 <a href="https://telegra.ph/HOW-TO-MIRROR-06-08"> 𝐇𝐎𝐖 𝐓𝐎 𝐌𝐈𝐑𝐑𝐎𝐑?</a>\n\n➩ <b>Don't Forget to Read</b> <a href="https://t.me/c/1295092084/127047"> 𝐑𝐔𝐋𝐄𝐒 📌</a>',disable_web_page_preview=True, parse_mode='HTMl')
         return 
     try:
         link = direct_link_generator(link)
